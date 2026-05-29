05186508aeac27a33ba3b03bfa5f3fd945c5556c
Rod <rodjohnguillen@gmail.com>
2026-05-30 06:00:00 +0800

    Finalize main launcher thread configuration profile entry point

---
+++ fan_system/main.py
@@ -0,0 +1,8 @@
+from fan_functions import FanSimulatorUI
+
+def main() -> None:
+    app_launcher = FanSimulatorUI()
+    app_launcher.start_registry_flow()
+
+if __name__ == "__main__":
+    main()
