a4d4f0615d87a798a508b2faf190d95e566af1a7
Rod <rodjohnguillen@gmail.com>
2026-05-30 22:00:00 +0800

    Define Car constructor structure accepting year_model and make attributes

--- car_system/car_Logic,.py
+++ car_system/car_Logic,.py
@@ -4,3 +4,10 @@ class Vehicle(ABC):
     @abstractmethod
     def display_telemetry(self) -> None:
         pass
+
+class Car(Vehicle):
+    def __init__(self, year_model: str, make: str) -> None:
+        """Initializes the car's year model and make from inputs, defaulting speed to 0."""
+        self.__year_model: str = str(year_model)
+        self.__make: str = str(make)
+        self.__speed: int = 0
